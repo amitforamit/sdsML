0ca9763576afaff0571632e22ed38609a4010dbe
Amit Kumar <amitforamit@gmail.com>
2018-08-25 20:11:19 +0530

    sds - day 2 - data preprocessing
    importing data
    and handling missing vales

--- sds - ML - Python/data_preprocessing.py
+++ sds - ML - Python/data_preprocessing.py
@@ -7,5 +7,15 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 #importing the dataset
-dataset = pd.read_csv('Data.csv')
+dataset = pd.read_csv('C:/Users/Admin/Desktop/Machine Learning/SDS- ML - A2Z/Machine Learning A-Z/Part 1 - Data Preprocessing/Data_Preprocessing/Data.csv')
 
+#creating matrix of features
+X = dataset.iloc[:,:-1].values
+y = dataset.iloc[:,3].values
+
+#taking care of missing data
+
+from sklearn.preprocessing import Imputer
+imputer = Imputer(missing_values ='NaN',strategy = 'mean',axis = 0)
+imputer = imputer.fit(X[:,1:3])
+X[:,1:3]= imputer.transform(X[:,1:3])
