1eef70efb7333ebeccc7b19045f3bd7f25768773
Amit Kumar <amitforamit@gmail.com>
2018-08-25 15:37:00 +0530

    sds - day1 - setup
    Importing libraries and dataset

---
+++ sds - ML - Python/data_preprocessing.py
@@ -0,0 +1,11 @@
+# Data Preprocessing
+
+# Importing the Libraries
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#importing the dataset
+dataset = pd.read_csv('Data.csv')
+
