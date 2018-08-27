6327209cdf6d76f86c948712af565a7a037b61d1
Amit Kumar <amitforamit@gmail.com>
2018-08-27 23:14:17 +0530

    sds - day 4 - data preprocessing
    train and test dataset split
    feature scalling

--- sds - ML - Python/data_preprocessing.py
+++ sds - ML - Python/data_preprocessing.py
@@ -28,3 +28,29 @@ onehotencoder = OneHotEncoder(categorical_features = [0])
 X = onehotencoder.fit_transform(X).toarray()
 labelencoder_y = LabelEncoder()
 y = labelencoder_y.fit_transform(y)
+
+#Splitting the dataset into Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2,random_state =0)
+
+#Feature scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- sds - ML - R/data_preprocessing.R
+++ sds - ML - R/data_preprocessing.R
@@ -16,3 +16,23 @@ dataset$Country = factor(dataset$Country,
 dataset$Purchased = factor(dataset$Purchased,
                          levels = c('No','Yes'),
                          labels = c(0,1))
+#Splitting the dataset into Training set and Test set
+#install.packages('caTools')
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Purchased,SplitRatio=0.8)
+training_set = subset(dataset,split == TRUE)
+test_set =subset(dataset,split == FALSE)
+
+#Feature scaling
+training_set[,2:3] = scale(training_set[,2:3])
+test_set[,2:3] = scale(test_set[,2:3])
+
+
+
+
+
+
+
+
+
